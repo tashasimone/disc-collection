362b82c5365fcdcdc5019013c018f2db610cd3fa
Tasha Simone <tashaspeterson@gmail.com>
2018-11-02 16:47:49 -0400

    Starting to put in disc and collection classes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      someone said this is an app
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -23,6 +22,42 @@ class App extends Component {
         </header>
       </div>
     );
+  }
+}
+
+class Disc extends Component {
+  render() {
+    const title = "Samantha";
+    return (
+      <div className="Disc">
+        <img src={logo} className="Disc-cover" alt="disc cover" />
+        <h2 className="Disc-title">{title}</h2>
+        <ul className="Disc-info">
+          <li className="Disc-type">Type: {this.props.type}</li>
+          <li className="Disc-year">Year: {this.props.year}</li>
+          <li className="Disc-genre">Genre: {this.props.genre}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Collection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      discs: Array(5).fill(null)
+    };
+  }
+
+  renderDisc(i) {
+    return <Disc value={i} />;
+  }
+
+  render() {
+    <div>
+      {this.renderDisc(0)}
+    </div>
   }
 }
 
