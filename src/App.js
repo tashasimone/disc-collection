8eb0462b9661df2341f23e3f3354e1b1ea8ea053
Tasha Simone <tashaspeterson@gmail.com>
2018-11-09 01:00:14 -0500

    Sorting implemented, add disc works within instance

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class Disc extends Component {
           <li className="Disc-type">Type: {this.props.type}</li>
           <li className="Disc-year">Year: {this.props.year}</li>
           <li className="Disc-genre">Genre: {this.props.genre}</li>
+          <li className="Disc-rating">Rating: {this.props.rating}</li>
         </ul>
       </div>
     );
@@ -24,17 +25,40 @@ class Collection extends React.Component {
     this.state = {
       discs: DiscData.collection
     };
-    console.log(DiscData.collection);
-    this.sort();
+    this.sort = this.sort.bind(this);
+    this.addDisc = this.addDisc.bind(this);
   }
 
   renderDisc(disc) {
-    return <Disc key={disc.title} title={disc.title} type={disc.type} year={disc.year} genre={disc.genre} />;
+    return <Disc key={disc.title} title={disc.title} type={disc.type} year={disc.year} genre={disc.genre} rating={disc.rating}/>;
   }
 
   render() {
     return (
       <div className="Collection">
+      <form>
+        <select value={this.state.sortBy} onChange={this.sort}>
+          <option value="title">title</option>
+          <option value="year">year</option>
+          <option value="genre">genre</option>
+          <option value="rating">rating</option>
+          <option value="type">disc type</option>
+        </select>
+
+        <p>ADD DISC</p>
+        <label>Title: <input type="text" name="title" onChange={e => this.setState({ title: e.target.value})}></input></label>
+        <label>Disc Type: 
+          <select value="DVD" name="type" onChange={e => this.setState({ type: e.target.value})}>
+            <option value="DVD">DVD</option>
+            <option value="Blu-Ray">Blu-Ray</option>
+            <option value="CD">CD</option>
+          </select>
+        </label>
+        <label>Year: <input type="text" name="year" onChange={e => this.setState({ year: e.target.value})}></input></label>
+        <label>Genre: <input type="text" name="genre" onChange={e => this.setState({ genre: e.target.value})}></input></label>
+        <label>Rating: <input type="text" name="rating" onChange={e => this.setState({ rating: e.target.value})}></input></label>
+        <input type="button" value="Add Disc" onClick={this.addDisc}/>
+      </form>
         {this.state.discs.map((disc) =>
             this.renderDisc(disc)
         )}
@@ -42,8 +66,22 @@ class Collection extends React.Component {
     );
   }
 
-  sort(sortBy = "title") {
-    console.log(this.state.discs);
+  addDisc(event){
+    this.state.discs.push({
+      title: this.state.title,
+      type: this.state.type,
+      year: this.state.year,
+      genre: this.state.genre,
+      rating: this.state.rating
+    });
+    this.sort({
+      target: {
+        value: this.state.sortBy
+      }
+    });
+  }
+  sort(event) {
+    let sortBy = event.target.value;
     this.state.discs.sort((a, b) => {
       let comparison = 0;
       if (a[sortBy] > b[sortBy]) {
@@ -53,7 +91,7 @@ class Collection extends React.Component {
       }
       return comparison;
     });
-    console.log(this.state.discs);
+    this.setState({state: this.state, sortBy: sortBy});
   }
 }
 
